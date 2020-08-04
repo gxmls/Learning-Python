0af158d5011ab84436485183d3c828e5faeaaa57
gxmls <54612072+gxmls@users.noreply.github.com>
2020-08-04 22:51:31 +0800

    Create 20200804-change type.py

---
+++ 20200804-change type.py
@@ -0,0 +1,11 @@
+a=100
+b=3.14
+c='hello world'
+d=True
+print(float(a)) #float() is a function for converting a variant to float
+print(str(b))
+print(bool(c)) # if variant c contains any chracter, bool(c) will be True, otherwise it will be False
+print(int(d)) # True=1 and False=0
+print(chr(97)) # chr() is a function for converting a number into a character. Number 97 in character table is letter 'a'
+print(ord('a')) #ord('') is a function for converting a character into a number. Letter 'a' in charcter table is 97
+#Note:if you want to convert letter a into a number, you should write ord('a'), not ord(a). Remember the ''
