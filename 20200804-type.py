9b7de6b0792d83e1a7fcbf9a622ad7acfdf64666
gxmls <54612072+gxmls@users.noreply.github.com>
2020-08-04 22:54:03 +0800

    Create 20200804-type.py

---
+++ 20200804-type.py
@@ -0,0 +1,9 @@
+a=100
+b=3.13
+c='hello world'
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#Note: type() is a function for checking the type of a variant, for example, 100 is int, 3.14 is float, 'hello world' is str, and True is bool
