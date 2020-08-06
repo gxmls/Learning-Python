10353d0d9959f2f3eec8c0aa90edd1426ef01ea5
gxmls <54612072+gxmls@users.noreply.github.com>
2020-08-06 14:09:07 +0800

    Create 20200806-CRAPS_v2.py
    in this version, the programme could count how many times you bet.

---
+++ 20200806-CRAPS_v2.py
@@ -0,0 +1,44 @@
+from random import randint
+money=1000
+debt_num=0
+while money>0:
+    print(f'You have total assets {money}')
+    go_on=False
+    while True: #Note: 'while True' means if the condition is true, the circular will be continued until meets break.
+        debt=int(input('Please place a bet: '))
+        if 0<debt<=money:
+            break
+    debt_num+=1
+    first=randint(1,6)+randint(1,6)
+    print(f'Player shook out {first} points')
+    if first==7 or first==11:
+        print(f'Player Wins!\n')
+        money+=debt
+    elif first==2 or first==3 or first==12:
+        print(f'Dealer Wins!\n')
+        money-=debt
+    else:
+        go_on=True
+    while go_on: #Note: go_on=True
+        go_on=False
+        current=randint(1,6)+randint(1,6) #Note: The next round begins if there was no winner in the first round
+        print(f'Player shook out {current} points')
+        if current==7:
+            print('Dealer Wins!\n')
+            money-=debt
+        elif current==first:
+            print('Player Wins!\n')
+            money+=debt
+        else:
+            go_on=True #Note: Back to the beginning until money <=0
+print('Sorry, you are bankrupt! \n')
+print(f'You totally bet {debt_num} times.\n')
+
+
+
+
+
+        
+
+
+    
