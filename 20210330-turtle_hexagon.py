7742888618e91e369018b668791562b27a902724
gxmls <54612072+gxmls@users.noreply.github.com>
2021-03-30 15:11:06 +0800

    Add files via upload

---
+++ 20210330-turtle_hexagon.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.penup()
+turtle.pensize(2)
+turtle.pencolor("darkblue")
+turtle.pendown()
+for i in range(6):
+    turtle.fd(100)
+    turtle.left(60)
+turtle.done()
---
+++ 20210330-turtle_square.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.penup()
+turtle.pensize(2)
+turtle.pendown()
+for i in range(4):
+    turtle.fd(100)
+    turtle.left(90)
+turtle.done()
