70754f40404093a322c30bdb50d26110e4b939c4
gxmls <54612072+gxmls@users.noreply.github.com>
2021-04-28 23:52:31 +0800

    Add files via upload

---
+++ Case2/Case2wordcloudv2.py
@@ -0,0 +1,15 @@
+import jieba
+import wordcloud
+from imageio import imread #能够读取一个图片文件
+mask=imread("heart.png") #词云会生成在有颜色的部位
+f=open("新时代中国特色社会主义.txt","r",encoding="utf-8")
+t=f.read()
+f.close()
+ls=jieba.lcut(t)
+for word in ls:
+    if len(word)==1:
+        ls.remove(word)
+txt=" ".join(ls)
+w=wordcloud.WordCloud(font_path="msyh.ttc",mask=mask,width=1000,height=700,background_color="white",max_words=50,stopwords="的")
+w.generate(txt)
+w.to_file("grwordcloud_heart.png")
