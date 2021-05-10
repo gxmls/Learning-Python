e09b0aa1163248d73d6c2d13ee856beb61e20e34
gxmls <54612072+gxmls@users.noreply.github.com>
2021-05-10 20:05:04 +0800

    Update Case12wordcloud.py

--- Case12/Case12wordcloud.py
+++ Case12/Case12wordcloud.py
@@ -4,7 +4,8 @@ f=open("新时代中国特色社会主义.txt","r",encoding="utf-8")
 t=f.read()
 f.close()
 ls=jieba.lcut(t)
-for word in ls:
+ls2=ls[:] #如果不复制一个列表直接从原始列表中删除元素会出现漏删
+for word in ls2:
     if len(word)==1:
         ls.remove(word)
 txt=" ".join(ls)
